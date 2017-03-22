871511f2f993cbc11ee16aebed16f29bab6d0e7b
MohamedMSI <msi.moussi@gmail.com>
2017-03-23 00:26:33 +0100

    Implement the socket.io for real time wishes.

--- REST.js
+++ REST.js
@@ -1,5 +1,48 @@
-var mysql   = require("mysql");
+var mysql = require("mysql");
 
+
+var io = require('socket.io').listen(3002);
+// var elasticsearch = require('elasticsearch');
+
+
+// Connect to localhost:9200 and use the default settings
+// var client = new elasticsearch.Client();
+
+// Connect the client to one node
+/*var client = elasticsearch.Client({
+  host: 'localhost:9200'
+});*/
+
+// Connect to this host using https, basic auth,
+// a path prefix, and static query string values
+/*var client = new elasticsearch.Client({
+  host: 'http://localhost:9200',
+  log: 'trace'
+});*/
+
+/*client.ping({
+  requestTimeout: 30000,
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});*/
+var socketCount = 0;
+
+io.sockets.on('connection', function(socket) {
+  // Socket has connected, increase socket count
+  socketCount++;
+  // Let all sockets know how many are connected
+  io.sockets.emit('users connected', socketCount);
+
+  socket.on('disconnect', function () {
+    // Decrease the socket count on a disconnect, emit
+    socketCount--;
+    io.sockets.emit('users connected', socketCount)
+  });
+});
 function REST_ROUTER(router,connection,md5) {
     var self = this;
     self.handleRoutes(router,connection,md5);
@@ -59,6 +102,7 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
         res.json({"Error" : true, "Message" : "Error executing MySQL query: "+err});
       } else {
         res.json({"Error" : false, "Message" : "Wish Added !", "wish" : rows});
+        io.sockets.emit('new wish', {'wish': req.body.wish, 'id': rows.insertId});
       }
     });
   });
